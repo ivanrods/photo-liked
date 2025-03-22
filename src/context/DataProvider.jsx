1be26f69e8dba52322394a26a385e4f53a082c66
Ivan <contaivanrodrigues@gmail.com>
2025-03-21 21:23:15 -0300

    Mudança no nome do projeto

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -1,15 +1,16 @@
 import { createContext, useState, useEffect } from "react";
+// eslint-disable-next-line react-refresh/only-export-components
 export const DataContext = createContext();
 
 function DataProvider({ children }) {
 
   const [dataLike, setDataLike] = useState(() => {
     const savedData = localStorage.getItem("like");
-    return savedData ? JSON.parse(savedData) : []; // Verificação extra para evitar erro
+    return savedData ? JSON.parse(savedData) : []; 
   });
 
   useEffect(() => {
-    if (dataLike.length > 0) {
+    if (dataLike.length >= 0) {
       localStorage.setItem("like", JSON.stringify(dataLike));
     }
   }, [dataLike]);
