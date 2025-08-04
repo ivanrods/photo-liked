eed2a5a39f9821324c4da975146504d29af9a4ec
Ivan <contaivanrodrigues@gmail.com>
2025-08-04 16:07:14 -0300

    feat: add get user route and delete user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,3 +34,23 @@ exports.login = async (req, res) => {
     res.status(500).json({ error: "Erro interno no login" });
   }
 };
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ error: "Usuário não encontrado" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar perfil do usuário" });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.user.id);
+    res.status(200).json({ message: "Usuário deletado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao deletar usuário" });
+  }
+};
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,12 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Token não enviado" });
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token)
+    return res.status(401).json({ error: "Acesso negado. Token ausente." });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
+    req.user = decoded;
     next();
   } catch {
     res.status(401).json({ error: "Token inválido" });
