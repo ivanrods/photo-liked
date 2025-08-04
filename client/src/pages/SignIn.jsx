1ea602cc8bf3610d864c69f01e5d29089c1a5f6c
Ivan <contaivanrodrigues@gmail.com>
2025-08-04 11:34:25 -0300

    feat: add handleSubmit in pages

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,29 @@
+import { loginUser } from "../api/auth";
 import { Link } from "react-router-dom";
 import Submit from "../components/Submit";
 import InputForm from "../components/InputForm";
 import Title from "../components/Title";
 import Form from "../components/Form";
+import { useState } from "react";
 
 function SignIn() {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await loginUser(form);
+    alert(res.token ? "Login bem-sucedido!" : res.error);
+  };
   return (
     <div className="h-screen">
-      <main className="bg-gray-100 w-full flex flex-col items-center justify-center h-full gap-8">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-100 w-full flex flex-col items-center justify-center h-full gap-8"
+      >
         <Title title=" Bem vindo de volta" />
         <Form>
           <InputForm
@@ -15,6 +31,7 @@ function SignIn() {
             type="email"
             label="Email"
             id="email"
+            onChange={handleChange}
           />
 
           <InputForm
@@ -22,6 +39,7 @@ function SignIn() {
             type="password"
             label="Senha"
             id="senha"
+            onChange={handleChange}
           />
           <Link to="/profile">
             <Submit value="Entrar" />
@@ -29,7 +47,7 @@ function SignIn() {
         </Form>
 
         <Link to="/signUp">Não tem uma conta?</Link>
-      </main>
+      </form>
     </div>
   );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,35 +1,57 @@
+import { registerUser } from "../api/auth";
 import { Link } from "react-router-dom";
 import Submit from "../components/Submit";
 import InputForm from "../components/InputForm";
 import Title from "../components/Title";
 import Form from "../components/Form";
+import { useState } from "react";
 function SignUp() {
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await registerUser(form);
+    alert(res.message || res.error);
+  };
   return (
     <div className="h-screen">
-      <main className="bg-gray-100 w-full flex flex-col items-center justify-center h-full gap-8">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-100 w-full flex flex-col items-center justify-center h-full gap-8"
+      >
         <Title title="Crie sua conta" />
         <Form>
-          <InputForm placeholder="Digite seu nome" type="text" label="Nome" id="nome" />
+          <InputForm
+            placeholder="Digite seu nome"
+            type="text"
+            label="Nome"
+            id="nome"
+            onChange={handleChange}
+          />
           <InputForm
             placeholder="Digite seu email"
             type="email"
             label="Email"
             id="email"
+            onChange={handleChange}
           />
           <InputForm
             placeholder="Digite sua senha"
             type="password"
             label="Senha"
             id="senha"
+            onChange={handleChange}
           />
 
-          <Link to="/profile">
-            <Submit value="Criar conta" />
-          </Link>
+          <Submit value="Criar conta" />
         </Form>
 
         <Link to="/signIn">Já tem uma conta?</Link>
-      </main>
+      </form>
     </div>
   );
 }
