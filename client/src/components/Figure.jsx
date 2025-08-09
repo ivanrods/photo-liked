0f688098196cd7b0675ee18554b907173cfccded
Ivan <contaivanrodrigues@gmail.com>
2025-08-09 10:46:47 -0300

    add: download img

--- client/src/components/Figure.jsx
+++ client/src/components/Figure.jsx
@@ -1,5 +1,6 @@
 import { FaHeart } from "react-icons/fa";
 import { useRef, useState, useEffect } from "react";
+import { FaDownload } from "react-icons/fa6";
 
 function Figure({ onClick, src, alt, description, like, mod, onLike }) {
   const imgRef = useRef(null);
@@ -63,12 +64,43 @@ function Figure({ onClick, src, alt, description, like, mod, onLike }) {
         </p>
         <div className="flex items-center justify-between mt-2">
           <span className="bg-gray-200 px-2 rounded-md">Photo</span>
-          <FaHeart
-            onClick={onLike}
-            className={`text-xl ${
-              like ? "text-gray-600" : "text-gray-300"
-            } cursor-pointer`}
-          />
+          <div className="flex gap-4 justify-center items-center">
+            {mod && (
+              <FaDownload
+                onClick={async () => {
+                  try {
+                    const response = await fetch(src, { mode: "cors" });
+                    if (!response.ok) throw new Error("Erro ao baixar imagem");
+
+                    const blob = await response.blob();
+                    const url = window.URL.createObjectURL(blob);
+
+                    const link = document.createElement("a");
+                    link.href = url;
+                    link.download = alt
+                      ? alt.replace(/\s+/g, "_") + ".jpg"
+                      : "image.jpg";
+
+                    document.body.appendChild(link);
+                    link.click();
+
+                    document.body.removeChild(link);
+                    window.URL.revokeObjectURL(url);
+                  } catch (error) {
+                    alert("Falha ao baixar a imagem");
+                    console.error(error);
+                  }
+                }}
+                className="text-gray-600 text-xl"
+              />
+            )}
+            <FaHeart
+              onClick={onLike}
+              className={`text-xl ${
+                like ? "text-gray-600" : "text-gray-300"
+              } cursor-pointer`}
+            />
+          </div>
         </div>
       </article>
     </div>
